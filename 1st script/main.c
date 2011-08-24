21a718104505acdbe19720cd7c71e50b88a479d0
TaikiMMT <taiki.mmt@gmail.com>
2011-08-24 18:50:23 +0200

    First chapter configurator script

--- 1st script/main.c
+++ 1st script/main.c
@@ -6,13 +6,13 @@ int main(int argc, char *argv[])
     char syntaxe[40] = {0}, extansion[5] = {0}, syntaxeDoublage[10] = {0};
     int page = 0, i = 0, j = 0, pageDoublee = 0, cmbPDouble = 0, pageDouble[20] = {-1}, numerotation = 1, credit = 0;
     FILE* config = NULL;
-    printf("Quelle est la syntaxe des fichiers?\n-> ");
+    printf("What is the file's syntax?\n-> ");
     scanf("%s", syntaxe);
-    printf("Quelle est l'extansion (sans point)?\n-> ");
+    printf("What is the extansion (withou dots)\n-> ");
     scanf("%s", extansion);
-    printf("Combien y a t'il de pages (fichiers)? \n(pages doubles = une page, la page des credits compte comme une)\n-> ");
+    printf("How many pages (files)? \n(double pages = one page, credits are a page)\n-> ");
     scanf("%d", &page);
-    printf("Y a t-il des pages doubles et si oui, combien, sinon, tapez 0.\nTapez ensuite leur position, en sautant un ligne\n-> ");
+    printf("is it some double pages and if so, how many, if not, type 0.\nType after them position, with lines skipping\n-> ");
     scanf("%d", &cmbPDouble);
     if(cmbPDouble != 0 && cmbPDouble < 20)
     {
@@ -22,10 +22,10 @@ int main(int argc, char *argv[])
             printf("-> ");
             scanf("%d", &pageDouble[i]);
         }
-        printf("Quelle est la syntaxe liant les deux pages? (juste a la place du '_')\n%s&numero_de_page_n 1&_____&numero_de_page_n 2&.%s\n-> ", syntaxe, extansion);
+        printf("What is the syntax who link the two pages? (in place of '_')\n%s&page_numer_one&_____&page_number_2&.%s\n-> ", syntaxe, extansion);
         scanf("%s", syntaxeDoublage);
     }
-    printf("La page de credit est-elle inclue dans la numerotation?\n0: Inclue dans la numerotation ou a la fin\n1: Non inclut dans la numerotation & en premiere position.\nATTENTION, SI VOUS FAITES 1, VOUS DEVREZ METTRE LE NOM DU FICHIER MANUELLEMENT\n->");
+    printf("Does credits to be included inside the numbering?\n0: Include or at the end\n1: No-include and on the first position.\nWARNING, IF YOU TYPE 1, YOU'LL NEED WRITE THE FILE'S NAME MANUALY\n->");
     scanf("%d", &credit);
     config = fopen("config.dat", "w+");
     fprintf(config, "     \n");
